bf96c1a3b3c77834b877f7b7a2b8b8e6c19f11fd
KvotheTankian <35543750+KvotheTankian@users.noreply.github.com>
2018-01-18 18:09:25 +0100

    Update dicx.py
    Añadidos modo 1337 (leet) y ligeros cambios.

--- dicx.py
+++ dicx.py
@@ -1,63 +1,122 @@
 # -*- coding: cp1252 -*-
-# GENERADOR DE DICCIONARIOS DE CONTRASEÑAS CREADO POR @KvotheTankian (forum.xen0x.org)
-# Dicx v1.0
-# Falta leet mode, para la siguiente versión
+# GENERADOR DE DICCIONARIOS DE CONTRASEÃ‘AS CREADO POR @KvotheTankian (forum.xen0x.org)
+# Dicx v1.1 (aÃ±adidos leet mode y pequeÃ±os detalles)
+
 import datetime
 from itertools import permutations
+
 print('@KvotheTankian')
 print('forum.xen0x.org')
 print('\n\n\n                SI NO QUIERES INTRODUCIR UNA PALABRA, SIMPLEMENTE DALE A ENTER \n')
-print('                           (Introduce las keywords en minúsculas)\n')
+print('                           (Introduce las keywords en minÃºsculas)\n')
+
 p1 = raw_input("> Nombre: ")
 p2 = raw_input("> Apellido: ")
 p3 = raw_input("> Segundo apellido: ")
 p4 = raw_input("> Apodo, otro nombre: ")
-p5 = raw_input("> Día: ")
+p5 = raw_input("> DÃ­a: ")
 p6 = raw_input("> Mes: ")
-p7 = raw_input("> Año: ")
-p8 = raw_input("> 2 últimas cifras del año: ")
+p7 = raw_input("> AÃ±o: ")
+p8 = raw_input("> 2 Ãºltimas cifras del aÃ±o: ")
 p9 = raw_input("> Inicial 1: ")
 p10 = raw_input("> Inicial 2: ")
 p11 = raw_input("> Inicial 3: ")
-p12 = raw_input("> Another word: ")
-p13 = raw_input("> Another word: ")
-p14 = raw_input("> Another word: ")
-p15 = raw_input("> Another word: ")
-p16 = raw_input("> Another word: ")
-p17 = raw_input("> Another word: ")
+p12 = raw_input("> Otra keyword: ")
+p13 = raw_input("> Otra keyword: ")
+p14 = raw_input("> Otra keyword: ")
+p15 = raw_input("> Otra keyword: ")
+p16 = raw_input("> Otra keyword: ")
+p17 = raw_input("> Otra keyword: ")
 print('\n\n\n\n')
 
 allwords = [p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17]
 words = []
-
 for word in allwords:
     if len(word) > 0:
         words.append(word)
 
-lmin = int(input('> Menor número de keywords en una contraseña: '))
-lmax = int(input('> Mayor número de keywords en una contraseña: '))
-lmax += 1
-
+kmin = int(input('> Menor nÃºmero de keywords en una contraseÃ±a: '))
+kmax = int(input('> Mayor nÃºmero de keywords en una contraseÃ±a: '))
+kmax += 1
+lenmax = int(input('> MÃ¡xima longitud de una contraseÃ±a (caracteres): '))
+             
 time = str(datetime.datetime.now().strftime("%y.%m.%d-%H.%M.%S"))
-txtname = p1+'_'+time+'.txt'
+txtname = p1.capitalize()+'-'+time+'.txt'
+dic = open(txtname,'w')
 
-print('\n\n')
-print('[+] Creando diccionario y agregando contraseñas...')
-n = 0
-with open(txtname,'w') as dic:
-    for i in range(lmin,lmax):
+#leetmode
+def leet(pw):
+    pw = pw.replace('e','3')
+    pw = pw.replace('E','3')
+    pw = pw.replace('i','1')
+    pw = pw.replace('I','1')
+    pw = pw.replace('o','0')
+    pw = pw.replace('O','0')
+    pw = pw.replace('a','4')
+    pw = pw.replace('A','4')
+    pw = pw.replace('s','5')
+    pw = pw.replace('S','5')
+    pw = pw.replace('t','7')
+    pw = pw.replace('T','7')
+    dic.write(pw)
+
+
+#crear diccionario sin leet mode
+def noleet():
+    for i in range(kmin,kmax):
         for a in permutations(words,i):
             password = ''.join(a) + '\n'
             passwordcap = password.capitalize()
-            dic.write(password)
-            n += 1
-            if password != passwordcap:
-                dic.write(passwordcap)
-                n += 1
+            if len(password) <= lenmax:
+                dic.write(password)
+                if password != passwordcap:
+                    dic.write(passwordcap)
+
+#crear diccionario con leet mode
+def leetmode():
+    for i in range(kmin,kmax):
+        for a in permutations(words,i):
+            password = ''.join(a) + '\n'
+            passwordcap = password.capitalize()
+            if len(password) <= lenmax:
+                dic.write(password)
+                leet(password)
+                if password != passwordcap:
+                    dic.write(passwordcap)
+                    leet(passwordcap)
+                            
+
+mode = raw_input('''> Activar leet mode? [S/N] (se crearian contraseÃ±as cambiando letras
+  por nÃºmeros, por ejemplo: hola = h0l4): ''')
+
+print('\n\n')
+print('[+] Creando diccionario y agregando contraseÃ±as...')
+
+if mode == 'no' or mode == 'n' or mode == 'N' or mode == 'NO' or mode == 'No' or mode == '':
+    noleet()
+elif mode == 'si' or mode == 's' or mode == 'SI' or mode == 'S' or mode == 'Si':
+    leetmode()
+dic.close()
+
 print('[+] Diccionario creado exitosamente!')
-print('[+] El diccionario contiene %s contraseñas')%n
-print('\n\n\n')
-print('(El diccionario se ha creado en la misma carpeta que la script)')
+print('(El diccionario se ha creado en la misma carpeta que la script)\n\n')
+
+#contar contraseÃ±as creadas
+def contar():
+    with open(txtname,'r') as pwlist:
+        n = 0
+        for i in pwlist:
+            n += 1
+        print('[+] El diccionario creado contiene %s contraseÃ±as')%n
+
+contarpw = raw_input('> Desea contar las contraseÃ±as que se han generado? [escribe s si quieres, y dale a enter si no]: ')
+if contarpw == 's' or contarpw == 'S':
+    contar()
+
+print('\n\n')
+print('Feliz cracking! :)')
+
+
 
 
 
